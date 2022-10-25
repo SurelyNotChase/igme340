0b2b46a26490da6e53fa26f3268a189d46e22e2c
SurelyNotChase <chase.ler@outlook.com>
2022-10-24 21:00:30 -0400

    trying

--- bible-app/bibleapp/lib/main.dart
+++ bible-app/bibleapp/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'not Giphy Finder'),
+      home: const MyHomePage(title: 'Bible App'),
     );
   }
 }
@@ -32,51 +32,71 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late String searchUrl = 'https://api.scripture.api.bible/v1/bibles';
   late String apiKey = 'a2659cf791469685d502c666d9ca0ad4';
+  late List<Widget> bibleList = [];
+  String dropdownValue = "cmn";
+  List<String> languageList = <String>['eng','ara','cmn','deu'];
 
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-
-    super.dispose();
+  void initState() {
+    getBibles();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          ElevatedButton(
-            child: Text("reset"),
-            onPressed: () async {
-              doSearch('text', 'text');
-            },
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            ElevatedButton(
+              child: Text("call"),
+              onPressed: () async {
+                getBibles();
+              },
+            ),
+            DropdownButton(
+              value: dropdownValue,
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items: languageList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+        body: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: bibleList,
+            ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 
-  Future doSearch(String input, limit) async {
-    // print('dosearch');
+  Future getBibles() async {
     String url = searchUrl;
-    // url = url.replaceAll("{0}", apiKey);
-    // url = url.replaceAll("{1}", input);
-
-    var client = http.Client();
-    try {
-      var response = await client.post(
-          Uri.https('https://api.scripture.api.bible', '/v1/bibles'),
-          headers: {'api-key': 'a2659cf791469685d502c666d9ca0ad4'});
-      var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-      var uri = Uri.parse(decodedResponse['uri'] as String);
-      print(await client.get(uri));
-    } finally {
-      client.close();
-    }
-
-    var respone = await http.get(Uri.parse(url));
+    setState(() {
+        bibleList.clear();
+      });
+    var respone = await http.get(Uri.parse(url), 
+    headers: {
+      "api-key": apiKey,
+      "language":dropdownValue
+      });
 
     var jData = jsonDecode(respone.body);
+    // var id = jData[0].data[0].id;
+
+    // ignore: avoid_print
+    for (var i = 0; i <= jData['data'].length; i++) {
+      setState(() {
+        bibleList.add(Text(jData['data'][i]["name"]));
+      });
+    }
   }
 }
